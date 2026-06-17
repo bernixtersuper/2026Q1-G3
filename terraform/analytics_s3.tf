937fa396c1fb928d90f3012960eb35bbf5d7480a
ctepedino <ctepedino@itba.edu.ar>
2026-06-17 12:25:02 -0300

    Remove moved declarations

--- terraform/analytics_s3.tf
+++ terraform/analytics_s3.tf
@@ -9,18 +9,3 @@ module "s3_analytics_ml" {
   source = "./modules/s3-private"
   name   = "${local.name_prefix}-analytics-silver"
 }
-
-moved {
-  from = module.s3_analytics
-  to   = module.s3_analytics_events
-}
-
-moved {
-  from = module.s3_analytics_bronze
-  to   = module.s3_analytics_events
-}
-
-moved {
-  from = module.s3_analytics_silver
-  to   = module.s3_analytics_ml
-}
